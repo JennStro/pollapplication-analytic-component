1323aa98293e77e90fa4444689ab20cc677f9b42
JennStro <jennys1994@gmail.com>
2020-10-21 13:01:54 +0200

    Add simple receiver

--- src/main/java/com/pollapplicationanalyticcomponent/PollapplicationAnalyticComponentApplication.java
+++ src/main/java/com/pollapplicationanalyticcomponent/PollapplicationAnalyticComponentApplication.java
@@ -1,10 +1,29 @@
 package com.pollapplicationanalyticcomponent;
 
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @SpringBootApplication
 public class PollapplicationAnalyticComponentApplication {
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+											 MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames("poll-app-queue");
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollapplicationAnalyticComponentApplication.class, args);
