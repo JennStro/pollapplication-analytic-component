7f55d908515ff8d05218b9b030062cc24c0be16f
JennStro <jennys1994@gmail.com>
2020-10-21 12:45:37 +0200

    Do not ignore pom and src

---
+++ src/main/java/com/pollapplicationanalyticcomponent/PollapplicationAnalyticComponentApplication.java
@@ -0,0 +1,13 @@
+package com.pollapplicationanalyticcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollapplicationAnalyticComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollapplicationAnalyticComponentApplication.class, args);
+	}
+
+}
