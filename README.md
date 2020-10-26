ce416e8aa8869162de1d4bfbc5b53722aef6dcc8
JennStro <jennys1994@gmail.com>
2020-10-26 19:24:48 +0100

    Add mongo db dependecy

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ### RabbitMQ Listener 
 ____
 
-A simple RabbitMQ listener configured by Spring Boot that prints out the message it has been given. 
+A simple RabbitMQ listener configured by Spring Boot that prints out the message it has been given.
+
+This is a part of project [PollApplication](https://github.com/chris2402/PollApplication) from DAT250-course. 
 
 
 **How to use**
--- src/main/java/com/pollapplicationanalyticcomponent/PollapplicationAnalyticComponentApplication.java
+++ src/main/java/com/pollapplicationanalyticcomponent/PollapplicationAnalyticComponentApplication.java
@@ -1,9 +1,5 @@
 package com.pollapplicationanalyticcomponent;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
