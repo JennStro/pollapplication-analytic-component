a487c3a75feaf7b59ff968ed944432c3772a2fc8
JennStro <jennys1994@gmail.com>
2020-10-27 11:19:16 +0100

    Save to mongodb database

--- src/main/java/com/pollapplicationanalyticcomponent/Poll.java
+++ src/main/java/com/pollapplicationanalyticcomponent/Poll.java
@@ -5,6 +5,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Date;
+
 @Document(collection = "FinishedPolls")
 public class Poll {
 
@@ -13,10 +16,35 @@ public class Poll {
 
     private String question;
 
+    private int yes;
+    private int no;
+
+    private Instant received;
+
     public Poll() {};
 
-    public Poll(Long id, String question) {
+    public Poll setId(Long id) {
         this.id = id;
+        return this;
+    }
+
+    public Poll setQuestion(String question) {
         this.question = question;
+        return this;
+    }
+
+    public Poll setYes(int yes) {
+        this.yes = yes;
+        return this;
+    }
+
+    public Poll setNo(int no) {
+        this.no = no;
+        return this;
+    }
+
+    public Poll setReceived(Instant received) {
+        this.received = received;
+        return this;
     }
 }
--- src/main/java/com/pollapplicationanalyticcomponent/Receiver.java
+++ src/main/java/com/pollapplicationanalyticcomponent/Receiver.java
@@ -1,16 +1,35 @@
 package com.pollapplicationanalyticcomponent;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
 public class Receiver {
 
-    @Autowired PollRepository pollRepository;
+  @Autowired PollRepository pollRepository;
 
-    @RabbitListener(queues = PollapplicationAnalyticComponentApplication.queueName)
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+  @RabbitListener(queues = PollapplicationAnalyticComponentApplication.queueName)
+  public void receiveMessage(String message) {
+    System.out.println("Received " + message);
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      JsonNode pollObject = mapper.readTree(message);
+      JsonNode votesObject = mapper.readTree(String.valueOf(pollObject.get("votes")));
+      pollRepository.save(
+          new Poll()
+              .setId(pollObject.get("id").longValue())
+              .setQuestion(pollObject.get("question").textValue())
+              .setYes(votesObject.get("yes").intValue())
+              .setNo(votesObject.get("no").intValue())
+              .setReceived(Instant.now()));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
     }
+  }
 }
